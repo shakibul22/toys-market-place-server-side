24db3a747d063a582659b32614386b06da8f7bfe
Shakibul Islam <shakibulislam684@gmail.com>
2023-05-19 22:18:45 +0600

    and   added for authentication

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://toys-market-place:HlQ6YjOvNC9yWVCu@cluster0.hyww9ng.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hyww9ng.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
