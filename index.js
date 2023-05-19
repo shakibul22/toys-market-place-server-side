d13de4b1be6353478ccdc43cfabe7eb6367c3c04
Shakibul Islam <shakibulislam684@gmail.com>
2023-05-19 22:03:18 +0600

    mongodb added

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = "mongodb+srv://toys-market-place:HlQ6YjOvNC9yWVCu@cluster0.hyww9ng.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
